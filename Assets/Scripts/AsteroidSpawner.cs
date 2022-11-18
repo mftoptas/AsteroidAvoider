119b12b039cfe2931aeb065c362e1e91663ed2b4
Muhammet Furkan Toptaş <m.furkantoptas@gmail.com>
2022-11-18 19:41:46 +0300

    Continuing to develop the game
    Continuing to develop the game

---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] asteroidPrefabs;
+    [SerializeField] private float secondsBetweenAsteroids = 1.5f;
+    [SerializeField] private Vector2 forceRange;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

3010853ce554499233344e302438ba4540a9fb5d
Muhammet Furkan Toptaş <m.furkantoptas@gmail.com>
2022-11-18 11:02:17 +0300

    Continuing to develop the game
    Continuing to develop the game

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+
+        if (playerHealth == null) { return; }
+
+        playerHealth.Crash();
+    }
+}

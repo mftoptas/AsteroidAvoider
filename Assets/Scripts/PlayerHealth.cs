c48b5e8868cd61285f6ad625815f0a7e5e224dbb
Muhammet Furkan Toptaş <m.furkantoptas@gmail.com>
2022-11-21 00:37:41 +0300

    Continuing to develop the game
    Continuing to develop the game

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField] private GameOverHandler gameOverHandler;
     public void Crash()
     {
+        gameOverHandler.EndGame();
+        
         gameObject.SetActive(false);
     }
 }
